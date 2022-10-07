ff0e0ea6d9800c58ecbc63c32f9404b18e259354
Haroune Hadid <harounehadidbusiness@gmail.com>
2022-10-07 12:08:19 +0100

    Add RaceCar class

---
+++ src/RaceCar.java
@@ -0,0 +1,23 @@
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class RaceCar extends Car {
+
+    boolean isTurbo;
+
+    public RaceCar(
+        String brand, 
+        String factory, 
+        String color, 
+        int passengersNum, 
+        Date buyingDate, 
+        int idPlate,
+        ArrayList tiresNum, 
+        int horsePower, 
+        float pickupTime,
+        boolean isTurbo
+    ) {
+        super(brand, factory, color, passengersNum, buyingDate, idPlate, tiresNum, horsePower, pickupTime);
+        this.isTurbo = isTurbo;
+    }  
+}
