0f0bfc4a69e81aabab87fcbf8f902164d17e7c2b
Haroune Hadid <harounehadidbusiness@gmail.com>
2022-10-07 11:28:56 +0100

    Add Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,47 @@
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Vehicle {
+    
+    String brand;
+    String factory;
+    String color;
+    int passengersNum;
+    Date buyingDate;
+    int idPlate;
+    ArrayList tiresNum;
+    int horsePower;
+
+    public Vehicle(
+        String brand,
+        String factory,
+        String color,
+        int passengersNum,
+        Date buyingDate,
+        int idPlate,
+        ArrayList tiresNum,
+        int horsePower
+    ) {
+
+        this.brand = brand;
+        this.factory = factory;
+        this.color = color;
+        this.passengersNum = passengersNum;
+        this.buyingDate = buyingDate;
+        this.idPlate = idPlate;
+        this.tiresNum = tiresNum;
+        this.horsePower = horsePower;
+    }
+
+    // This is just a random function
+    public int calculateHorsePower(float x) {
+        return (int)((x * Math.pow(10, 3) * 1.43));
+    }
+
+    // I'll change it later
+    public LocalDateTime calculatePeriod(Date buyingDate) {
+        return LocalDateTime.now();
+    }
+}
